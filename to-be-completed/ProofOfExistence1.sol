a866714a3e538c98e6986eb9b2a5a9c071349b82
netanmangal <imnetanmangal@gmail.com>
2022-09-17 21:48:58 +0530

    Add: ProofOfExistence1.sol

--- to-be-completed/ProofOfExistence1.sol
+++ to-be-completed/ProofOfExistence1.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.7;
 
 contract ProofOfExistence1 {
       // state
@@ -7,13 +8,12 @@ contract ProofOfExistence1 {
       // calculate and store the proof for a document
       // *transactional function*
       function notarize(string memory document) public {
-        
+        proof = proofFor(document);
       }
 
       // helper function to get a document's sha256
       // *read-only function*
       function proofFor(string memory document) public pure returns (bytes32) {
+          return sha256( abi.encodePacked(document) );
       }
-
 }
-
